9eba72a8dd36aa11e8584ce5d2515f87dab08a7a
alx-yng <alexyoung0119@gmail.com>
2024-03-30 12:35:36 -0400

    add execute script

--- background.js
+++ background.js
@@ -1,35 +1,30 @@
-// Function to send message to content script
-function sendMessageToContentScript(tabId, message) {
-    chrome.tabs.executeScript(tabId, { file: 'contentScript.js' }, () => {
-      chrome.tabs.sendMessage(tabId, message, (response) => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-          return;
-        }
-        // Handle response from content script
-        if (response && response.content) {
-          console.log(response.content);
-        } else {
-          console.log("No <p> tag content found.");
-        }
-      });
-    });
-  }
-  
-  // Listener for extension action click
-  chrome.action.onClicked.addListener(async (tab) => {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === "ON" ? "OFF" : "ON";
-  
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-  
-    // Send a message to content script to get <p> tag content
-    sendMessageToContentScript(tab.id, { action: 'getPTagContent' });
+// Listener for extension action click
+chrome.action.onClicked.addListener(async (tab) => {
+  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  // Next state will always be the opposite
+  const nextState = prevState === "ON" ? "OFF" : "ON";
+
+  // Set the action badge to the next state
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
   });
-  
+
+  if (nextState === "ON") {
+    // Insert the CSS file when the user turns the extension on
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        const pTags = document.getElementsByTagName("p");
+        let textContent = "";
+        // const wordMatchRegExp = /[^\s]+/g;
+        for (let i = 0; i < pTags.length; i++) {
+          textContent += pTags[i].textContent + " ";
+        }
+        console.log(textContent.trim());
+      },
+    });
+  } else if (nextState === "OFF") {
+  }
+});
