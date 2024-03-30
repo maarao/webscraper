8fb3689531a4d27ead8d8dbd119b6a5b1a1ce873
alx-yng <alexyoung0119@gmail.com>
2024-03-30 12:19:07 -0400

    added p tag fetch

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
-const extensions = 'https://developer.chrome.com/docs/extensions'
-const webstore = 'https://developer.chrome.com/docs/webstore'
-
 chrome.action.onClicked.addListener(async (tab) => {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  // Next state will always be the opposite
+  const nextState = prevState === "ON" ? "OFF" : "ON";
 
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
+  // Set the action badge to the next state
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  const pTags = document.getElementsByTagName("p");
+
+  // `document.querySelector` may return null if the selector doesn't match anything.
+  if (pTags) {
+    let textContent = "";
+    const wordMatchRegExp = /[^\s]+/g; // line.Regular expression
+    for (let i = 0; i < pTags.length; i++) {
+      textContent += pTags[i].textContent + " ";
+    }
+  }
+
+  console.log(textContent);
 });
