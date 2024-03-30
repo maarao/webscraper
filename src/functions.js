97a0784641291499ca23be1574bb61ddcfede2e5
alx-yng <alexyoung0119@gmail.com>
2024-03-30 15:20:11 -0400

    fixed url send with react

---
+++ src/functions.js
@@ -0,0 +1,18 @@
+export function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  chrome.tabs.query(queryOptions, ([tab]) => {
+    if (chrome.runtime.lastError) console.error(chrome.runtime.lastError);
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    sendUrl(tab);
+  });
+}
+
+const sendUrl = (tab) => {
+  let url = tab.url;
+  // fetch("http://localhost:5000/process", {
+  //   method: "POST",
+  //   body: JSON.stringify({ url }),
+  // });
+  console.log(url);
+  console.log(tab.url);
+};
