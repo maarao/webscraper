b47d8d9c1e7499a3e038bac19c22f3bb431ab412
Simon Grishin <simon.grishin@gmail.com>
2024-03-30 20:25:59 -0400

    fixed text

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import re
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from ML.classification import Classification
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -20,20 +21,6 @@ def home():
 
 @app.route('/process', methods=['POST'])
 
-def preprocess_text(text):
-
-        text = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text)
-        # Tokenize the text into words
-        words = word_tokenize(text.lower())
-
-        words = [word for word in words if word not in string.punctuation]
-
-        # Remove stop words
-        stop_words = stopwords.words('english')
-        words = [word for word in text.split() if word not in (stop_words)]
-        words = " ".join([word for word in words if word.isalpha()]).lower()    
-        
-        return words
 
 def predict():
     URL = request.get_json()['text']
@@ -44,18 +31,31 @@ def predict():
     parsed_url = urlparse(URL).netloc
     base_url = parsed_url[4:]
 
-    
-
     text = []
     # Find all <p> elements and print their text
     for paragraph in soup.find_all('p'):
         text.append(preprocess_text(paragraph.get_text()))
-    # Initialize the ML classification
-    print(obj.label_classification(text))
 
-    print(text)
+    # Initialize the ML classification
+    # print(obj.label_classification(text))
+
+    print("Text", text)
     return jsonify({'text': text})
 
+def preprocess_text(text):
+
+    text = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text)
+    # Tokenize the text into words
+    words = word_tokenize(text.lower())
+
+    words = [word for word in words if word not in string.punctuation]
+
+    # Remove stop words
+    stop_words = stopwords.words('english')
+    words = [word for word in text.split() if word not in (stop_words)]
+    words = " ".join([word for word in words if word.isalpha()]).lower()    
+        
+    return words
 
 if __name__ == '__main__':
     app.run(debug=True)
