e154cbfb0dc32e3d52ec371b9066878a22f14d58
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-31 11:05:55 -0400

    Revert "fix: fixed relevancy calculation"
    This reverts commit 5d09bd076586e543865bfecc38b2301d33bbd8bc.

--- app.py
+++ app.py
@@ -45,17 +45,14 @@ def predict():
     p_mean, p_std = polarity(sent_text) # Between [-1, 1] where -1 is negative and 1 is positive
     subject = subjectivity(sent_text) # between 0 - 1 (1 is opinionated), (0 is factual)
     relevancy, images_urls = fact_check(URL)
-    calc_rel = 0
     if relevancy > 1:
-        calc_rel = 1
-    else:
-        calc_rel = relevancy
+        relevancy = 1
 
     bias_classificiation_name = ['Bias', 'Conspiracy', 'Fake', 'BS', 'Satire', 'Hate', 'Junksci', 'State']
     bias_classification_percentages = [0.6, 0, 0.1, 0.2, 0.7, 0.5, 0, 1]
     classification_score = real + bias_classification_percentages[bias]
 
-    score = (calc_rel * .2 + (real) * .15 + (0.4 * (0.5 * (1 - subject) + .3 * (1 - p_std) + .2 * (1 - abs(p_mean)))) + bias_classification_percentages[bias] * .25)
+    score = (relevancy * .2 + (real) * .15 + (0.4 * (0.5 * (1 - subject) + .3 * (1 - p_std) + .2 * (1 - abs(p_mean)))) + bias_classification_percentages[bias] * .25)
 
     print("Score: ",score)
     # print("Text", text)
