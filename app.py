9d7c8e61292aa50d31d8911fdadec07b94e5bc9d
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-31 10:25:18 -0400

    fix: calculated score

--- app.py
+++ app.py
@@ -47,10 +47,9 @@ def predict():
     relevancy, images_urls = fact_check(URL)
     if relevancy > 1:
         relevancy = 1
-    classification_score = real + bias_classification_percentages[bias]
-    print(classification_score)
 
     bias_classificiation_name = ['Bias', 'Conspiracy', 'Fake', 'BS', 'Satire', 'Hate', 'Junksci', 'State']
+    classification_score = real + bias_classification_percentages[bias]
     bias_classification_percentages = [0.6, 0, 0.1, 0.2, 0.7, 0.5, 0, 1]
 
     score = (relevancy * .2 + real * .15 + (0.4 * (0.5 * (1 - subject) + .3 * (1 - p_std) + .2 * (1 - abs(p_mean)))) + bias_classification_percentages[bias] * .25)
