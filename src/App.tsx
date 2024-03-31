e8d8c541bd1d6931bef9f63b30417b7bb5980ef1
alx-yng <alexyoung0119@gmail.com>
2024-03-31 02:21:33 -0400

    fix: fixed loading skeletons

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import MainRating from "./components/MainRating.js";
 import SubRatings from "./components/SubRatings.js";
 import { Skeleton } from "./components/ui/skeleton.js";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "./components/ui/button.js";
 
 function App() {
   const [score, setScore] = useState(0);
   const [data, setData] = useState(JSON.parse(localStorage.getItem("data")));
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchData();
@@ -29,7 +30,10 @@ function App() {
     <main className='border border-red-500 flex flex-col items-center py-4 max-h-[350px] w-[680px] p-4 gap-4 animate-slide-from-top-with-fade'>
       <div className='flex justify-between w-full items-center px-4'>
         <h1 className='text-4xl font-light tracking-wide'>Title</h1>
-        <ThemeToggle />
+        <div className='flex item-center gap-2'>
+          <Button variant='outline'>?</Button>
+          <ThemeToggle />
+        </div>
       </div>
       <Separator className='-mt-2' />
       <div className='flex gap-8 items-center px-4'>
@@ -42,13 +46,15 @@ function App() {
         </div>
         <div>
           {loading ? (
-            <div className='grid grid-rows-3 grid-cols-2 gap-x-3 gap-y-2 place-items-center'>
-              <Skeleton className='h-8 w-32' />
-              <Skeleton className='h-8 w-24' />
-              <Skeleton className='h-8 w-32' />
-              <Skeleton className='h-8 w-24' />
-              <Skeleton className='h-8 w-32' />
-              <Skeleton className='h-8 w-24' />
+            <div className='grid grid-rows-8 grid-cols-1 gap-y-1 scale-90 w-64'>
+              <Skeleton className='h-6 w-12' />
+              <Skeleton className='h-6 w-full' />
+              <Skeleton className='h-6 w-32' />
+              <Skeleton className='h-6 w-full' />
+              <Skeleton className='h-6 w-[6.5rem]' />
+              <Skeleton className='h-6 w-full' />
+              <Skeleton className='h-6 w-24' />
+              <Skeleton className='h-6 w-full' />
             </div>
           ) : (
             <SubRatings polarity={0} />
